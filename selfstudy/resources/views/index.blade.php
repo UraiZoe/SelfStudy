b336a08e35889a3df22c6a605871ab0014a54bda
UraiZoe <urazoe366@hengersor.hu>
2025-05-31 00:10:18 +0200

    dfg

---
+++ selfstudy/resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.layout")
+<!-- Fejléc kiszedés -->
+ 
+@section("title", "Üdvözlünk!")
+<!-- Cím adás az oldalnak változó által -->
+
+@section("content")
+<!-- Kontent kiszedés -->
+
+<main>
+    <h1></h1>
+    
+</main>
+@endsection
+<!-- Lezárás -->
